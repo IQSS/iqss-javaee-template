07e1ae534d31295efd48392deca653304b2b2853
Philip Durbin <philip_durbin@harvard.edu>
2013-02-01 10:27:10 -0500

    update from Solrj 3.6.2 to 4.1.0

--- examples/web/hello1/src/main/java/javaeetutorial/web/hello1/Solr.java
+++ examples/web/hello1/src/main/java/javaeetutorial/web/hello1/Solr.java
@@ -4,10 +4,11 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.params.ModifiableSolrParams;
 import java.net.MalformedURLException;
 
 @Named
@@ -26,26 +27,29 @@ public class Solr {
     public void setResultString(String search_term) throws MalformedURLException, SolrServerException {
         /**
          * @fixme what if solr is down?
-         * @fixme stop using deprecated API
          */
-        CommonsHttpSolrServer solr = new CommonsHttpSolrServer("http://localhost:8983/solr");
-
-        ModifiableSolrParams params = new ModifiableSolrParams();
-        //params.set("q", "cat:electronics");
-        //params.set("q", "*:*");
-        params.set("q", search_term);
-        params.set("defType", "edismax");
-        params.set("start", "0");
-
-        QueryResponse response = solr.query(params);
-        SolrDocumentList results = response.getResults();
+        String url = "http://localhost:8983/solr";
+        /*
+         Warning from https://wiki.apache.org/solr/Solrj
+         
+         HttpSolrServer is thread-safe and if you are using the following constructor,
+         you *MUST* re-use the same instance for all requests.  If instances are created on
+         the fly, it can cause a connection leak. The recommended practice is to keep a
+         static instance of HttpSolrServer per solr server url and share it for all requests.
+         See https://issues.apache.org/jira/browse/SOLR-861 for more details
+         */
+        SolrServer server = new HttpSolrServer(url);
+        SolrQuery query = new SolrQuery();
+        query.setQuery(search_term);
+        QueryResponse rsp = server.query(query);
+        SolrDocumentList docs = rsp.getResults();
+        this.resultString = docs.size() + " results";
         /**
          * @todo show the actual results, not just the number of results
          */
-        this.resultString = results.size() + " results";
+        //this.resultString = results.size() + " results";
         //for (int i = 0; i < results.size(); ++i) {
         //    System.out.println(results.get(i));
         //}
-
     }
 }
